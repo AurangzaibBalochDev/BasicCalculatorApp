e0306a39a98295d2c27c5fe5b6a100c7768d8a5e
Aurangzaib <zaibbuzdar3@gmail.com>
2024-10-01 11:54:44 -0700

    commit

--- app/src/main/java/com/example/firstzaib/CalculatorViewModel.kt
+++ app/src/main/java/com/example/firstzaib/CalculatorViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
+
 class CalculatorViewModel : ViewModel() {
     private val _equationText = MutableLiveData("")
     val equationText: LiveData<String> = _equationText
@@ -19,13 +20,14 @@ class CalculatorViewModel : ViewModel() {
                     _equationText.value = ""
                     _resultText.value = "0"
                 }
+
                 "C" -> {
                     if (it.isNotEmpty()) {
                         _equationText.value = it.substring(0, it.length - 1)
                     }
                 }
+
                 "=" -> {
-                    // Only calculate result when "=" is pressed
                     try {
                         val result = calculateResult(_equationText.value.toString())
                         _equationText.value = result
@@ -34,15 +36,14 @@ class CalculatorViewModel : ViewModel() {
                         _resultText.value = "Error"
                     }
                 }
+
                 else -> {
                     if (IsOperator(btn[0]) && it.isNotEmpty() && IsOperator(it.last())) {
-                        // Replace last operator if another operator is clicked
                         _equationText.value = it.substring(0, it.length - 1) + btn
                     } else {
                         _equationText.value = it + btn
                     }
 
-                    // Try calculating the result, but only if the last character isn't an operator
                     if (!IsOperator(btn[0])) {
                         try {
                             _resultText.value = calculateResult(_equationText.value.toString())
